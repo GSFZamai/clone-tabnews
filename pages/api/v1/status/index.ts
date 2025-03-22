4e00a48b6b3f362efbabf79444b35a70b1dace86
GSFZamai <gsfzamai@gmail.com>
2025-03-22 11:40:01 -0300

    Added `baseUrl` on `tsconfig.json`

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,6 +1,5 @@
 import { NextApiResponse, NextApiRequest } from "next";
-import database from "@/infra/database";
-
+import database from "infra/database";
 async function status(request: NextApiRequest, response: NextApiResponse) {
   console.log((await database.query("SELECT 1 + 1 AS SUM;")).rows);
   return response.status(200).json({ message: "funcionando!" });
